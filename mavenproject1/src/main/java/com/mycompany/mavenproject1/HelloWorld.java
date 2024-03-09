62f35f7edec2fa75a03ac39ca633ae47e4c07b82
Jorge <Jorge@LAPTOP-13O2AL1N>
2024-03-09 20:53:56 -0300

    comentario de autor

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/HelloWorld.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/HelloWorld.java
@@ -1,5 +1,9 @@
 package com.mycompany.mavenproject1;
 
+/**
+ *
+ * @author Jorge
+ */
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Olá, Mundo!");
